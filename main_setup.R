d84148de9b3880f978c988f3102ead40a680bd11
TomNook21 <thomas.nuck@edu.fh-joanneum.at>
2025-10-13 20:10:41 +0200

    Einlesen und Überblick der Daten

--- main_setup.R
+++ main_setup.R
@@ -1,4 +1,13 @@
 # Project Setup
-#Test by Tom
-#hallo manuel
+#aktivieren der Packages für Daten einlesen und bereinigen
+#Bitte nicht vergessen die packages unter Tools vorab zu installieren !
+library(readr)
+library(dplyr)
+library(tidyr)
+library(skimr)
 
+datensatz <- read.csv("C:/Github/payment-fraud-detection/Data/fraud_dataset_fhj25.csv")
+
+head((datensatz))
+
+skim(datensatz)
