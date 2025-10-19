832e12fc4d793c0b22182a0757021d4a83a5b7a5
raphiforever88 <raphael.suppan@edu.fh-joanneum.at>
2025-10-19 10:43:38 +0200

    Datenbereinigung

--- main_setup.R
+++ main_setup.R
@@ -11,3 +11,29 @@ datensatz <- read.csv("C:/Github/payment-fraud-detection/Data/fraud_dataset_fhj2
 head((datensatz))
 
 skim(datensatz)
+
+
+##check ob gleiche Datensätze vorhanden sind
+any(duplicated(datensatz))
+
+
+##Spalten unbenennen
+
+colnames(datensatz) <- c("date", "age", "profession","region", "account_balance", "num_debit","num_credit","num_pos","num_mov_conto","incoming payments","outgoing payments","product number","num_fraud")
+
+
+
+##2627 zeilen wurden gelöscht, schaupsi wann gehma bier trinken?
+datensatz <- datensatz %>%
+  filter(!(num_debit == 0 & 
+             num_credit == 0 & 
+             num_pos == 0 & 
+             num_fraud == 0))
+
+
+##Fokussierung u. sortieren der incoming payment spalte, Thomas is geil
+##Große Transaktionen sind wichtig
+
+df_sorted <- datensatz %>%
+  arrange(desc(`incoming payments`))
+
